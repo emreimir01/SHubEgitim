b56b58d875ed7e63d3c0b32219435ebee891ef0b
Emre <Emre@DESKTOP-915B97I>
2025-11-17 13:07:45 +0300

    17 - 11 - 25 Bootstrap bitti

---
+++ NetFrameworkMVCEgitimi/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NetFrameworkMVCEgitimi
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", // url kısmı uygulama tarayıcıda çalıştığında 
+                // site adından sonraki kısımda hangi controllerdeki hangi actionun çalışacağını ayarlar.
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // default kısmı uygulama çalıştığında
+                // varsayılan anasayfa olarak hangi sayfadan çalıştırılacağını ayarlar.
+            );
+        }
+    }
+}
